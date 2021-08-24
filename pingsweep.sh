dab5077ec43d9b0e84f53e36b366353706485aef
renatus-cartesius <cartesius.absolute@gmail.com>
2021-08-24 19:08:53 +0300

    Getting uid with c file

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,1 +1,14 @@
-ping google.com -c1 -W 1
+#!/bin/bash
+
+#configure the target`s ip segment in three octs
+ip=192.168.0
+
+min=0
+max=20
+
+for (( oct=$min; oct <= max; oct++)) ; do
+	if ping -c1 -W 1 "$1.$oct" &> /dev/null
+		then echo "$1.$oct is up"; echo "$ip.$oct" >> targets.txt 
+		else echo "$1.$oct is down"
+	fi
+done
