48faba26655e2782eaf7957a295bfe5be48432cf
renatus-cartesius <cartesius.absolute@gmail.com>
2022-01-19 13:46:01 +0300

    added

---
+++ pwn_res/prg.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+	char b1[10], b2[10];
+	scanf("%s", b2);\
+	printf("buffer 1:%s \n buffer 2: %s\n", b1, b2);
+	return 0;
+}
